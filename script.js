033940a8f43025990da0379d1e305e941ea239a0
ManuFerrer094 <manuferrer094@gmail.com>
2024-02-09 13:34:00 +0100

    Modal

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const downloadLink = document.getElementById('downloadLink');
 downloadButton.addEventListener('click', function() {
   downloadLink.click();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('modal');
+    const githubStarBtn = document.getElementById('githubStarBtn');
+    const cerrarBtn = document.getElementById('cerrarBtn');
+    const screenWidthThreshold = 768;
+
+    if (window.innerWidth >= screenWidthThreshold) {
+        window.addEventListener('mousemove', function(e) {
+            if (e.clientY < 50) {
+                modal.style.display = 'flex';
+            }
+        });
+    }
+
+    githubStarBtn.addEventListener('click', function() {
+        window.open('https://github.com/ManuFerrer094/ManuFerrer094.github.io', '_blank');
+    });
+
+    cerrarBtn.addEventListener('click', function() {
+        closeModal();
+    });
+
+    function closeModal() {
+        modal.style.display = 'none';
+    }
+});
+
+
