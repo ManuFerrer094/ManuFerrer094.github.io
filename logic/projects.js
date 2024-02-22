8c73848427aa41b6d198df03bbe8d24829015d9d
ManuFerrer094 <manuferrer094@gmail.com>
2024-02-22 17:55:23 +0100

    Fetching projects

--- logic/projects.js
+++ logic/projects.js
@@ -1,32 +1,113 @@
 import { updateElementContent, generateShieldURL } from './utils.js';
 
-fetch('texts.json')
-  .then(response => response.json())
-  .then(data => {
-    const projectsData = data.projects;
-    updateElementContent('#projects h2', projectsData.title);
-    updateElementContent('#projects p', projectsData.description);
+const projectsPerPage = 4;
+let currentPage = 1;
 
-    const projectContainer = document.querySelector('#projects .row');
-    projectsData.items.forEach(project => {
-      const stackHtml = project.stack.map(tech => `<img src="${generateShieldURL(tech)}" alt="${tech}">`).join('');
-      const projectHtml = `
-        <div class="col-12 col-md-6 project-container">
-          <div class="project">
-            <h3>${project.name}</h3>
-            <img src="${project.image}" alt="${project.name}">
-            <p>${project.description}</p>
-            <div class="stack-utilizado">
-              <h4>Stack utilizado:</h4>
-              <div class="about-stack">${stackHtml}</div>
-            </div>
-            <div class="btn-container">
-              <a href="${project.githubLink}" class="btn project-button" target="_blank">Github</a>
-              <a href="${project.demoLink}" class="btn project-button" target="_blank">Demo</a>
-            </div>
-          </div>
-        </div>`;
-      projectContainer.insertAdjacentHTML('beforeend', projectHtml);
-    });
+// Fetch del archivo JSON de configuración
+fetch('./texts.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
   })
-  .catch(error => console.error('Error cargando el archivo JSON:', error));
+  .then(config => {
+    const { githubUsername } = config;
+
+    fetch(`https://api.github.com/users/${githubUsername}/repos`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(repos => {
+        const ownRepos = repos
+        .filter(repo => !repo.fork)
+        .sort((a, b) => {
+          if (a.homepage && b.homepage) {
+            return new Date(b.created_at) - new Date(a.created_at);
+          } else if (a.homepage) {
+            return -1;
+          } else if (b.homepage) {
+            return 1;
+          } else {
+            return new Date(b.created_at) - new Date(a.created_at);
+          }
+        });
+
+        const projectsData = {
+          title: "Proyectos propios",
+          description: "Aquí tienes mis proyectos de Github!",
+          items: ownRepos.map(repo => ({
+            name: repo.name,
+            description: repo.description,
+            stack: repo.topics,
+            githubLink: repo.html_url,
+            demoLink: repo.homepage,
+          }))
+        };
+
+        updateElementContent('#projects h2', projectsData.title);
+        updateElementContent('#projects p', projectsData.description);
+
+        const projectContainer = document.querySelector('#projects .row');
+        
+        const divideProjectsIntoPages = () => {
+          const startIndex = (currentPage - 1) * projectsPerPage;
+          const endIndex = startIndex + projectsPerPage;
+          return projectsData.items.slice(startIndex, endIndex);
+        };
+
+        const displayProjects = () => {
+            projectContainer.innerHTML = '';
+            const projectsToShow = divideProjectsIntoPages();
+            projectsToShow.forEach(project => {
+              let stackHtml = '';
+              if (project.stack.length > 0) {
+                stackHtml = `
+                  <div class="stack-utilizado">
+                    <h4>Stack utilizado:</h4>
+                    <div class="about-stack">${project.stack.map(tech => `<img src="${generateShieldURL(tech)}" alt="${tech}">`).join('')}</div>
+                  </div>`;
+              }
+          
+              const projectHtml = `
+                <div class="col-12 col-md-6 project-container">
+                  <div class="project">
+                    <h3>${project.name}</h3>
+                    <p>${project.description}</p>
+                    ${stackHtml}
+                    <div class="btn-container">
+                      <a href="${project.githubLink}" class="btn project-button" target="_blank">GitHub</a>
+                      ${project.demoLink ? `<a href="${project.demoLink}" class="btn project-button" target="_blank">Demo</a>` : ''}
+                    </div>
+                  </div>
+                </div>`;
+              
+              projectContainer.insertAdjacentHTML('beforeend', projectHtml);
+            });
+          };
+          
+        const createPaginatorButtons = () => {
+          const totalPages = Math.ceil(projectsData.items.length / projectsPerPage);
+          const paginator = document.querySelector('.paginator');
+          paginator.innerHTML = '';
+          
+          for (let i = 1; i <= totalPages; i++) {
+            const button = document.createElement('button');
+            button.textContent = i;
+            button.addEventListener('click', () => {
+              currentPage = i;
+              displayProjects();
+            });
+            paginator.appendChild(button);
+          }
+        };
+
+        displayProjects();
+        createPaginatorButtons();
+      })
+      .catch(error => console.error('Error cargando los repositorios de GitHub:', error));
+  })
+  .catch(error => console.error('Error cargando la configuración:', error));
